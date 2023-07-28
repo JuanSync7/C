1ad79068644a359e08ac06d6d7818dd493fc5d5f
JuanSync7 <kokshewjuan7@gmail.com>
2023-07-28 13:26:59 +0100

    Bracket Solver

--- Bracket_Solver/bracket_solver.c
+++ Bracket_Solver/bracket_solver.c
@@ -4,50 +4,66 @@
 
 void bracket_solver(const char* sentence) {
     int counter = 0;
+    int invalid_counter = 0;
     char brackets[] = "";
-    
-    for (int i = 0; i < strlen(sentence); i++) {
-        if(sentence[i] == "{" ){
+    char invalid_brackets[] = "";
+    printf("sentence length: %i\n",strlen(sentence));
+    int sen_len = sizeof(sentence);
+    for (int i = 0; i < sen_len; i++) {
+        printf("char: %c\n", sentence[i]);
+        if(sentence[i] == 123){
             brackets[counter]= sentence[i];
             counter++;
             
         }
-        else if(sentence[i] == "("){
+        else if(sentence[i] == 40){
             brackets[counter]= sentence[i];
             counter++;
         }
-        else if(sentence[i] == "["){
+        else if(sentence[i] == 91){
             brackets[counter]= sentence[i];
             counter++;
         }
-        else if(sentence[i] == "}"){
-            if(brackets[counter] == "{"){
-                brackets[counter] == "";
-                counter--;
+        else if(sentence[i] == 125){
+            counter--;
+            if(brackets[counter] == 123){
+                brackets[counter] = 0;
+                
             }
             else{
                 printf("invalid brackets.");
+                invalid_brackets[invalid_counter] = sentence[i];
+                invalid_counter++;
             }
         }
-        else if(sentence[i] == ")"){
-            if(brackets[counter] == "("){
-                brackets[counter] == "";
-                counter--;
+        else if(sentence[i] == 41){
+            counter--;
+            if(brackets[counter] == 40){              
+                brackets[counter]= 0;
+            }
+            else{
+                printf("invalid brackets.\n");
+                invalid_brackets[invalid_counter] = sentence[i];
+                invalid_counter++;
+            }
+        }
+        else if(sentence[i] == 93){
+            counter--;
+            if(brackets[counter] == 91){
+                brackets[counter] = 0;
+                
             }
             else{
                 printf("invalid brackets.");
+                invalid_brackets[invalid_counter] = sentence[i];
+                invalid_counter++;
             }
         }
-        else if(sentence[i] == "]"){
-            if(brackets[counter] == "["){
-                brackets[counter] == "";
-                counter--;
-            }
-            else{
-                printf("invalid brackets.");
-            }
-        }
+       
     }
+    printf("Counter: %i\n", counter);
+    printf("Final opening bracket list: %s\n", brackets);
+    printf("Invalid bracket list: %s\n", invalid_brackets);
 }
 
 int main() {
